b53d979920084387e382010ecf79c7a2c8f43ba2
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-07-17 14:33:55 -0400

    Added in console log of user vs computer wins

--- test.js
+++ test.js
@@ -31,12 +31,38 @@ main();
 // need function for computer choice (random)
 function getComputerChoice() {
     const choices = [`r`, `p`, `s`];
-    // Need to choose a random number from our array of choices
-    console.log(Math.random ());
+    // Need to choose a random number from our array of choices. (math.random)
+    // Our array is 3 choices, so we multiply by 3
+    // We need to round down to create a whole number (math.floor)
+    // Store the result in a variable, not a console log
+    const randomNumber = (Math.floor(Math.random () * 3));
+    // We don't want the number, we want the element.
+    return choices[randomNumber];
 }
 
 // Define game function
 // Game function needs to take in user input
 function game(userChoice) {
-
+    const computerChoice = getComputerChoice();
+    // Using a switch statement instead of if statement
+    switch (userChoice + computerChoice) {
+        // Let's take care of the scenarios in which the user will win
+        case `rs`:
+        case `pr`:
+        case `sp`:
+            console.log(`USER WINS!`);
+            break;
+        // Scenarios in which the user loses
+        case "sr":
+        case `rp`:
+        case `ps`:
+            console.log(`computer wins :(`);
+            break;
+        // Tie
+        case "rr":
+        case `pp`:
+        case `ss`:
+            console.log(`It's a tie!`);
+            break;
+    }
 }
