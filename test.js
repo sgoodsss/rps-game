04184eedf02821903048478b8b927c917d19df67
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-07-16 20:08:00 -0400

    Cached the DOM in JS

---
+++ test.js
@@ -0,0 +1,12 @@
+// Cached the DOM
+// Storing all elements and ids in variables so that we can use them later
+const userScore = 0;
+const computerScore= 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("comp-score");
+const scoreboard_div = document.querySelector(".scoreboard");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
