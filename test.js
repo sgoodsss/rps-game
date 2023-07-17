61c433718d5c08703b6bacc704260bceef84de0e
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-07-17 14:14:14 -0400

    added Event Listeners for user choice

--- test.js
+++ test.js
@@ -10,3 +10,33 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
+function main() {
+    // Add Event Listeners to each function
+    rock_div.addEventListener (`click`, function () {
+        game (`r`);
+    })
+
+    paper_div.addEventListener (`click`, function () {
+        game (`p`);
+    })
+
+    scissors_div.addEventListener (`click`, function () {
+        game (`s`);
+    })
+}
+
+// Running above function
+main();
+
+// need function for computer choice (random)
+function getComputerChoice() {
+    const choices = [`r`, `p`, `s`];
+    // Need to choose a random number from our array of choices
+    console.log(Math.random ());
+}
+
+// Define game function
+// Game function needs to take in user input
+function game(userChoice) {
+
+}
